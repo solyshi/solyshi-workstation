64fa625250addb3aaaf8446042d1ea0feaa17952
Chri1899 <c.b-1@outlook.de>
2026-04-22 07:56:12 +0200

    feat: updated sessionizer script to account for more levels of subdirs

--- dotfiles/qutebrowser/.config/qutebrowser/colors.py
+++ dotfiles/qutebrowser/.config/qutebrowser/colors.py
@@ -1,68 +1,68 @@
 # Matugen generated colors for qutebrowser
 
 # Webpage background (Dark mode support)
-c.colors.webpage.bg = "#12140e"
+c.colors.webpage.bg = "#0f1512"
 c.colors.webpage.preferred_color_scheme = "dark"
 c.colors.webpage.darkmode.enabled = (
     True  # Optional, falls du echtes Dark-Theming willst
 )
 
 # Completion (Vorschläge in der Befehlszeile)
-c.colors.completion.category.bg = "#33362e"
-c.colors.completion.category.fg = "#b1d18a"
+c.colors.completion.category.bg = "#303633"
+c.colors.completion.category.fg = "#88d6ba"
 c.colors.completion.category.border.top = (
-    "#33362e"
+    "#303633"
 )
 c.colors.completion.category.border.bottom = (
-    "#33362e"
+    "#303633"
 )
-c.colors.completion.even.bg = "#12140e"
-c.colors.completion.odd.bg = "#1a1c16"
-c.colors.completion.fg = "#e2e3d8"
-c.colors.completion.item.selected.bg = "#b1d18a"
-c.colors.completion.item.selected.fg = "#1f3701"
-c.colors.completion.match.fg = "#a0d0cc"
-c.colors.completion.scrollbar.bg = "#12140e"
-c.colors.completion.scrollbar.fg = "#b1d18a"
+c.colors.completion.even.bg = "#0f1512"
+c.colors.completion.odd.bg = "#171d1a"
+c.colors.completion.fg = "#dee4df"
+c.colors.completion.item.selected.bg = "#88d6ba"
+c.colors.completion.item.selected.fg = "#00382a"
+c.colors.completion.match.fg = "#a8cbe1"
+c.colors.completion.scrollbar.bg = "#0f1512"
+c.colors.completion.scrollbar.fg = "#88d6ba"
 
 # Context menu
-c.colors.contextmenu.menu.bg = "#12140e"
-c.colors.contextmenu.menu.fg = "#e2e3d8"
-c.colors.contextmenu.selected.bg = "#b1d18a"
-c.colors.contextmenu.selected.fg = "#1f3701"
+c.colors.contextmenu.menu.bg = "#0f1512"
+c.colors.contextmenu.menu.fg = "#dee4df"
+c.colors.contextmenu.selected.bg = "#88d6ba"
+c.colors.contextmenu.selected.fg = "#00382a"
 
 # Downloads
-c.colors.downloads.bar.bg = "#12140e"
-c.colors.downloads.start.bg = "#b1d18a"
-c.colors.downloads.start.fg = "#1f3701"
-c.colors.downloads.stop.bg = "#1f4e4b"
+c.colors.downloads.bar.bg = "#0f1512"
+c.colors.downloads.start.bg = "#88d6ba"
+c.colors.downloads.start.fg = "#00382a"
+c.colors.downloads.stop.bg = "#274b5d"
 c.colors.downloads.error.bg = "#ffb4ab"
 c.colors.downloads.error.fg = "#690005"
 
 # Hints (Die Buchstaben zum Springen)
-c.colors.hints.bg = "#b1d18a"
-c.colors.hints.fg = "#1f3701"
-c.colors.hints.match.fg = "#bfcbad"
+c.colors.hints.bg = "#88d6ba"
+c.colors.hints.fg = "#00382a"
+c.colors.hints.match.fg = "#b2ccc1"
 
 # Statusbar
-c.colors.statusbar.normal.bg = "#12140e"
-c.colors.statusbar.normal.fg = "#e2e3d8"
-c.colors.statusbar.insert.bg = "#a0d0cc"
-c.colors.statusbar.insert.fg = "#003735"
-c.colors.statusbar.command.bg = "#12140e"
-c.colors.statusbar.command.fg = "#e2e3d8"
-c.colors.statusbar.url.success.https.fg = "#b1d18a"
-c.colors.statusbar.url.hover.fg = "#bfcbad"
+c.colors.statusbar.normal.bg = "#0f1512"
+c.colors.statusbar.normal.fg = "#dee4df"
+c.colors.statusbar.insert.bg = "#a8cbe1"
+c.colors.statusbar.insert.fg = "#0c3446"
+c.colors.statusbar.command.bg = "#0f1512"
+c.colors.statusbar.command.fg = "#dee4df"
+c.colors.statusbar.url.success.https.fg = "#88d6ba"
+c.colors.statusbar.url.hover.fg = "#b2ccc1"
 
 # Tabs (Hier habe ich den "Waybar-Look" eingebaut)
-c.colors.tabs.bar.bg = "#12140e"
-c.colors.tabs.even.bg = "#1a1c16"
-c.colors.tabs.even.fg = "#c5c8ba"
-c.colors.tabs.odd.bg = "#1a1c16"
-c.colors.tabs.odd.fg = "#c5c8ba"
-c.colors.tabs.selected.even.bg = "#b1d18a"
-c.colors.tabs.selected.even.fg = "#1f3701"
-c.colors.tabs.selected.odd.bg = "#b1d18a"
-c.colors.tabs.selected.odd.fg = "#1f3701"
-c.colors.tabs.indicator.start = "#b1d18a"
+c.colors.tabs.bar.bg = "#0f1512"
+c.colors.tabs.even.bg = "#171d1a"
+c.colors.tabs.even.fg = "#bfc9c3"
+c.colors.tabs.odd.bg = "#171d1a"
+c.colors.tabs.odd.fg = "#bfc9c3"
+c.colors.tabs.selected.even.bg = "#88d6ba"
+c.colors.tabs.selected.even.fg = "#00382a"
+c.colors.tabs.selected.odd.bg = "#88d6ba"
+c.colors.tabs.selected.odd.fg = "#00382a"
+c.colors.tabs.indicator.start = "#88d6ba"
 c.colors.tabs.indicator
